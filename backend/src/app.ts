b61440e9bbd105724986ee4bf8f46cbae7c0e92d
BarreiroT <tomasmbarreiroi@gmail.com>
2022-09-29 23:10:05 -0300

    fix bug

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,7 @@
 import { config } from 'dotenv';
 config();
 
-import express, { Request, Response } from 'express';
-import cors from 'cors';
+import express, { NextFunction, Request, Response } from 'express';
 
 import { Application } from './application/application';
 import { CheckoutSystem } from './application/modules/Checkout/CheckoutSystem';
@@ -21,17 +20,11 @@ PostgresSource.initialize().then((source) => {
 
     app.locals.application = new Application(checkoutSystem);
 
-    app.use(
-        cors({
-            origin: process.env.CLIENT_ORIGIN,
-        }),
-    );
-
     app.use(express.json());
 
     app.use('/api', router);
 
-    app.use((error: HttpException, _req: Request, res: Response) => {
+    app.use((error: HttpException, _req: Request, res: Response, _next: NextFunction) => {
         const status = error.statusCode || 500;
         const message = status === 500 ? 'Ha ocurrido un error en el servidor. Vuelve a intentarlo.' : error.message;
 
@@ -39,7 +32,7 @@ PostgresSource.initialize().then((source) => {
             console.error(error);
         }
 
-        res.status(status).json({ message });
+        res.status(status).json({ message, errors: error.data });
     });
 
     app.listen(Number(process.env.PORT), () => console.log(`Application listening on port ${process.env.PORT}`));
